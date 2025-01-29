3ae74927405758b5ad6393cc888c2535978d88e5
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2025-01-29 22:40:36 +0500

    user queries added

---
+++ GraphQlDemo/Queries/UserQuery.cs
@@ -0,0 +1,21 @@
+﻿using GraphQlDemo.Models;
+using GraphQlDemo.Repositories;
+namespace GraphQlDemo.Queries
+{
+    public class UserQuery
+    {
+        private readonly UserRepository _repository;
+
+        public UserQuery(UserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [GraphQLName("getUsers")]
+        public IEnumerable<User> GetUsers() => _repository.GetAllUsers();
+
+
+        [GraphQLName("getUserById")]
+        public User GetUser(int id) => _repository.GetUserById(id);
+    }
+}
